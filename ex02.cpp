e76fc136cfe1fef9e278b40f0af9aa1710b2029e
herolava259 <56593142+herolava259@users.noreply.github.com>
2020-10-15 20:17:27 +0700

    Add files via upload

---
+++ ex02.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n =-1;
+	while(n > 0){
+		cin >> n;
+	}
+	cout << n;
+}
---
+++ factorical.cpp
@@ -0,0 +1,14 @@
+#include<bít/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n;
+	int fac = 1;
+	cin >> n;
+	for(int i =2; i <= n; i++){
+		fac *= i;
+	}
+	return o;
+	
+}
